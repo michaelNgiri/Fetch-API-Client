1ddf978c6394bc6a724280c38ff86990f4714b36
alveis <michaelngiri@gmail.com>
2018-09-10 14:57:07 +0100

    all new changes

--- scripts/questions.js
+++ scripts/questions.js
@@ -3,7 +3,8 @@ function submitQuestion(){
     const question = document.getElementById('ask-question-body').value;
     console.log(title);
     console.log(question);
-    const body =  'question_title'+'='+title+'&'+'question'+'='+question+'id'+'='+userId+'Authorization'+'='+Authorization;
+    console.log(Authorization);
+    const body =  'question_title'+'='+title+'&'+'question'+'='+question+'&id'+'=1'+'&Authorization'+'='+Authorization;
     const url = "http://localhost:3000/api/v1/questions";
     postQuery(url, body)
 }
@@ -27,6 +28,49 @@ function postQuery(url, body) {
         }).then(data => {
         // Work with JSON data here
         Authorization = data
+    }).catch(err => {
+        console.log(err);
+        // Do something for an error here
+    });
+}
+fetch(url).then(response => {
+    return response.json();
+}).then(data => {
+    // Work with JSON data here
+    console.log(data[0]);
+    document.getElementById('featured-question-title').innerHTML=data[0]['question_title'];
+    document.getElementById('featured-question-body').innerHTML=data[0]['question_body'];
+    document.getElementById('featured-question-id').setAttribute('value', data[0]['id']);
+    document.getElementById('featured-question-owner-id').setAttribute('value', data[0]['id']);
+    console.log(data[0]['id']);
+    return data;
+}).catch(err => {
+    console.log(err);
+    // Do something for an error here
+});
+
+function fetchAllQuestions(){
+    fetch('http://localhost:3000/api/v1/questions/').then(response => {
+        return response.json();
+    }).then(data => {
+        // Work with JSON data here
+        console.log(data);
+        let i;
+        for(i = 0; i < data.length; i++) {
+            const obj = data[i];
+
+            console.log(obj.id);
+         const question = {
+                id:obj.id,
+                owner_id:obj.user_id,
+                title:obj.question_title,
+             body:obj.question_body
+            };
+            // console.log(question);
+            const element = document.getElementById('questions-list');
+            const child1 = element.appendChild('li').innerHTML=question.question.title;
+        }
+        document.getElementById('el1').innerHTML=data;
     }).catch(err => {
         console.log(err);
         // Do something for an error here
